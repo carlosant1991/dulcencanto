b4b8f86f5705377ef055cb3e3c5510a637be85bc
Carlos Antonio <antodomicz2013@gmail.com>
2026-07-21 10:07:45 -0500

    Create preloader.js

---
+++ js/preloader.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+
+  const preloader = document.querySelector('.body__preloader');
+
+  if (preloader) {
+    preloader.classList.add('loaded');
+  }
+
+});
